f38f43ea09a08a6afd499edfcde939d953052812
unknown <sarawutsafe1234@gmail.com>
2025-02-15 08:19:04 +0700

    บันทึกการเปลี่ยนแปลงก่อน pull

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -14,11 +14,14 @@ public class EventController : ControllerBase
 
     private static readonly List<Event> Events = new List<Event>
     {
-        new Event { Id = 1, Title = "Meeting Team A", Start = new DateTime(2025, 7, 10, 9, 0, 0), End = new DateTime(2025, 7, 12, 17, 0, 0) },
-        new Event { Id = 2, Title = "Sale Team Report", Start = new DateTime(2025, 7, 15, 10, 30, 0), End = new DateTime(2025, 7, 16, 14, 0, 0) },
-        new Event { Id = 3, Title = "ประชุมทีม DevOpt", Start = new DateTime(2024, 10, 1, 9, 0, 0) }, 
-        new Event { Id = 4, Title = "ส่งงาน", Start = new DateTime(2025, 4, 14, 9, 30, 0) }, 
-        new Event { Id = 5, Title = "จัดเลี้ยง", Start = new DateTime(2025, 7, 26, 19, 0, 0) }
+            new Event { Id = 1, Title = "Meeting Team A", Start = new DateTime(2025, 2, 17, 9, 0, 0), End = new DateTime(2025, 2, 19, 17, 0, 0) },
+            new Event { Id = 2, Title = "Sale Team Report", Start = new DateTime(2025, 2, 22, 10, 30, 0), End = new DateTime(2025, 2, 23, 14, 0, 0) },
+            new Event { Id = 3, Title = "ประชุมทีม DevOpt", Start = new DateTime(2025, 2, 24, 9, 0, 0) }, 
+            new Event { Id = 4, Title = "ส่งงาน", Start = new DateTime(2025, 2, 27, 9, 30, 0) }, 
+            new Event { Id = 5, Title = "จัดเลี้ยง", Start = new DateTime(2025, 3, 2, 19, 0, 0) },
+            new Event { Id = 6, Title = "วันหยุด", Start = new DateTime(2025, 3, 2, 19, 0, 0) }
+
+
     };
 
     [HttpGet]
